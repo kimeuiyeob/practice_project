acf8ba97e3cc39e595e0db744ac42c515d40f849
김의엽 <euiyeob@gim-uiyeob-ui-MacBookPro.local>
2024-06-15 19:37:46 +0900

    war properties change

--- daeduk/src/main/java/com/daeduk/DaedukApplication.java
+++ daeduk/src/main/java/com/daeduk/DaedukApplication.java
@@ -2,10 +2,17 @@ package com.daeduk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DaedukApplication {
+public class DaedukApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DaedukApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DaedukApplication.class, args);
 	}
