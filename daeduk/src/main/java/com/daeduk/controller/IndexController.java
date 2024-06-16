7d9eb8d6c5930e34a6bf81f4d681e4ab1d80e299
김의엽 <euiyeob@gim-uiyeob-ui-MacBookPro.local>
2024-06-16 22:18:52 +0900

    commit

--- daeduk/src/main/java/com/daeduk/controller/IndexController.java
+++ daeduk/src/main/java/com/daeduk/controller/IndexController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
-
     @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("message", "Hello, Daeduk!");
         return "index";
     }
-
 }
